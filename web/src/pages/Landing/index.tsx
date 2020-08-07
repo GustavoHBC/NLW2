f8c7165db6947a41da94efd1f3854e4eb73bca96
GustavoHBC <gugaex99@gmail.com>
2020-08-06 21:47:38 -0300

    landingpage update

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+
 
 function Landing() {
     return (
@@ -8,7 +13,21 @@ function Landing() {
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy" />
-                    <h2>Sua plataforma</h2>
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+                
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
                 </div>
             </div>
         </div>
