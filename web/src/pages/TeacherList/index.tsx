b0127a7e431d594f6698583ba48b8304cdd8fe63
GustavoHBC <gugaex99@gmail.com>
2020-08-09 21:31:03 -0300

    TeacherList styling

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
@@ -13,6 +15,10 @@ function TeacherList() {
                         <img src={backIcon} alt="Voltar"/>
                     </Link>
                     <img src={logoImg} alt="Proffy"/>
+                </div>
+
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
                 </div>
             </header>
         </div>
