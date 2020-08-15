00f3711a51d1150f288031c88e7ecf84768c9b9a
GustavoHBC <gugaex99@gmail.com>
2020-08-15 20:21:00 -0300

    server: list filtering DONE

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -32,10 +32,20 @@ export default class ClassesController {
         const timeInMinutes = convertHourToMinutes(time);
 
         const classes = await db('classes')
+            // arrow function não cria um novo escopo, logo não é possível utilizar .this
+            // KnexJS: sempre que utilizar whereExists, utilizar .whereRaw()
+            .whereExists(function() { // SUBQUERY com joins
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            })
             .where('classes.subject', '=', subject)
             .join('users', 'classes.user_id', '=', 'users.id')
             // resulta inner joing vvv
-            .select(['classes.*', 'users;*']);
+            .select(['classes.*', 'users.*']);
 
         return response.json(classes);
     }
