39d22ea9562e9f022289e0fd8942ff652b86dcc4
GustavoHBC <gugaex99@gmail.com>
2020-08-22 17:33:00 -0300

    web: filtering teachers on teacherlist

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,27 +4,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/43932907?s=460&u=c5ee08d530eaf1cc3693c608669abc1944ed0313&v=4" alt="Gustavo Bedendo"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Gustavo Bedendo</strong>
-                    <span>WebDev</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br/><br/>
-                Sunt ducimus quibusdam ipsam enim explicabo. Nihil, ad incidunt dignissimos voluptate alias voluptatibus esse. Fuga ullam esse eveniet nostrum quibusdam cumque alias!
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type="button">
                     <img src={whatsappIcon} alt="Whatsapp"/>
